92e0f98d40ec2c81f7f6728001a4f0e1b35a9590
Samual Norman <samualtnorman@gmail.com>
2021-10-28 18:57:28 +0100

    remove debug `console.log()`

--- index.cjs
+++ index.cjs
@@ -2,8 +2,6 @@ const { loopWhile } = require("deasync")
 
 let esmModule
 
-console.log("hello")
-
 import("./index.js").then(exports => esmModule = exports)
 loopWhile(() => !esmModule)
 module.exports = esmModule
